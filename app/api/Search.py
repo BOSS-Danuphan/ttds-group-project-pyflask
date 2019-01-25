76d6922ad3a0fe66ac76823a568041307422b418
Adam DePauw <a.j.depauw@sms.ed.ac.uk>
2019-01-25 00:20:39 +0000

    Removing commented code.

--- app/api/Search.py
+++ app/api/Search.py
@@ -86,25 +86,4 @@ class SearchEngine:
         difference = list(set(lists[0]).difference(lists[1]))
         difference.sort()
         return difference
-
-
-    
-
-    # @staticmethod
-    # def search(query, limit=None):
-    #     '''Perform search in index by a given query'''
-    #     # Only example on how to call app_index_collection
-    #     # isloaded = app_index_collection._loaded
-    #     return {
-    #         'nDocucment': app_index_collection._count, # Example
-    #         'total': 0,
-    #         'data': []
-    #     }
-
-    # @staticmethod
-    # def searchsingle(document, query):
-    #     '''Perform search by given document and query'''
-    #     return {
-    #         'isRelevant': True, # Is this document relevant acording to a given query
-    #         'document': document
-    #     }
+        
