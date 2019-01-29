8b6809943e36354df0bec6b4e81aba3f79a29cc2
BOSS-Danuphan <bossmeej@gmail.com>
2019-01-29 20:22:27 +0000

    Modify error handling and add logs to Writer modules

--- app/models/AzureBlobWriter.py
+++ app/models/AzureBlobWriter.py
@@ -1,33 +1,46 @@
 import io, time
 from app import app
+import sys
+from datetime import datetime
 from azure.storage.blob import BlockBlobService, PublicAccess
 
 class AzureBlobWriter:
-    
+
     def __init__(self, file_path="index.txt"):
         # Create a new file on every instantiation
         epoch_time = str(time.time()).split('.')[0]
-        self.file_path = "{0}-{1}".format(file_path, epoch_time)
-        self.blob_service = BlockBlobService(account_name=app.config['AZURE_BLOB_ACCOUNT'], account_key=app.config['AZURE_BLOB_KEY'])
-        self.container_name = "ttds-indexes"
+        self._file_path = "{0}-{1}".format(file_path, epoch_time)
+        self._blob_service = BlockBlobService(account_name=app.config['AZURE_BLOB_ACCOUNT'], account_key=app.config['AZURE_BLOB_KEY'])
+        self._container_name = "ttds-indexes"
 
     def read(self):
+        content = None
         try:
             # Get the most recently written file with the specified name
             highest_blob = 0
-            blob_name = self.file_path
+            blob_name = self._file_path
 
-            container = self.blob_service.list_blobs(self.container_name)
+            container = self._blob_service.list_blobs(self._container_name)
+            print([c.name for c in container])
             for blob in container:
                 blob_order = int(blob.name.split("-")[1])
                 if blob_order > highest_blob:
                     highest_blob = blob_order
                     blob_name = blob.name
 
-            blob = self.blob_service.get_blob_to_text(self.container_name, blob_name)
-            return blob.content
+            blob = self._blob_service.get_blob_to_text(self._container_name, blob_name)
+            content = blob.content
         except:
-            return None
+            print("Unexpected error at {}.read: {}".format(__name__, sys.exc_info()))
+        else:
+            print("Read content from {} by {} at {}".format(self._file_path, __name__, datetime.now().strftime(app.config['DATETIME_FORMAT'])))
+        return content
 
     def write(self, content):
-        self.blob_service.create_blob_from_text(self.container_name, self.file_path, content)
+        print('test')
+        try:
+            self._blob_service.create_blob_from_text(self._container_name, self._file_path, content)
+        except:
+            print("Unexpected error at {}.write: {}".format(__name__, sys.exc_info()))
+        else:
+            print("Content has been written to {} by {} at {}".format(self._file_path, __name__, datetime.now().strftime(app.config['DATETIME_FORMAT'])))
--- app/models/FileWriter.py
+++ app/models/FileWriter.py
@@ -1,4 +1,7 @@
 import os
+import sys
+from datetime import datetime
+from app import app
 
 class FileWriter:
     _file_path = ""
@@ -7,12 +10,21 @@ class FileWriter:
         self._file_path = file_path
 
     def read(self):
+        content = None
         try:
             with open(self._file_path, "r") as in_file:
-                return in_file.read()
+                content = in_file.read()
         except FileNotFoundError:
-            return None
+            print("Read content from {} by {} at {}".format(self._file_path, __name__, datetime.now().strftime(app.config['DATETIME_FORMAT'])))
+        else:
+            print("Read content by {} at {}".format(__name__, datetime.now().strftime(app.config['DATETIME_FORMAT'])))
+        return content
 
     def write(self, content):
-        with open(self._file_path, "w+") as out_file:
-            out_file.write(content)
+        try:
+            with open(self._file_path, "w+") as out_file:
+                out_file.write(content)
+        except:
+            print("Unexpected error at {}.write: {}".format(__name__, sys.exc_info()))
+        else:
+            print("Content has been written to {} by {} at {}".format(self._file_path, __name__, datetime.now().strftime(app.config['DATETIME_FORMAT'])))
