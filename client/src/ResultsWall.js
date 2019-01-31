d23e37c189f1ff0529714f06b36dfefa58ed5847
BOSS-Danuphan <bossmeej@gmail.com>
2019-01-31 12:13:21 +0000

    Add option 'conversation':'none' to embed tweets

--- client/src/ResultsWall.js
+++ client/src/ResultsWall.js
@@ -6,8 +6,13 @@ class ResultsWall extends Component {
 
     // <TwitterTweetEmbed tweetId='653229520015749121' options={{'height': '400', 'width':400}}/>
     render() {
-        const tweetList = this.props.stTweets.map((tweet, index) => <li key={tweet}><TwitterTweetEmbed tweetId={tweet}/></li>);
-        const rtTweetList= this.props.rtTweets.map((tweet, index) => <li key={tweet}><TwitterTweetEmbed tweetId={tweet}/></li>);
+        const tweetOption = {'conversation': 'none'}
+        const tweetList = this.props.stTweets.map((tweet, index) =>
+            <li key={tweet}><TwitterTweetEmbed tweetId={tweet} options={tweetOption}/></li>
+        );
+        const rtTweetList= this.props.rtTweets.map((tweet, index) =>
+            <li key={tweet}><TwitterTweetEmbed tweetId={tweet} options={tweetOption}/></li>
+        );
         return (
             <div className="ResultsWall">
                 <div className="ResultsWall-static">
