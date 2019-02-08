9c432c569ae6fe60bbfe1f8951408f04afb90948
Maciej Wlazlo <s1248922@sms.ed.ac.uk>
2019-02-08 02:23:39 +0000

    Updated About,Help and Contact Pages

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import logo from './GitHub-Mark-32px.png';
 import './Contact.css';
 
 class ContactPage extends Component {
@@ -6,7 +7,17 @@ class ContactPage extends Component {
     render() {
         return (
             <div className="ContactPage">
-                <h2>Contact</h2>
+                <div className="Panel">
+                    <h3>Contact</h3>
+                    <ul>
+                        <li><p>Azizah</p><a href=""><img alt="GitHub Logo" src={logo}/></a></li>
+                        <li><p>Adam</p><a href="https://github.com/adamdotdev"><img alt="GitHub Logo" src={logo}/></a></li>
+                        <li><p>Pavlos</p><a href="https://github.com/pgogousis"><img alt="GitHub Logo" src={logo}/></a></li>
+                        <li><p>Hansun</p><a href="https://github.com/joanna350"><img alt="GitHub Logo" src={logo}/></a></li>
+                        <li><p>Danuphan</p><a href="https://github.com/BOSS-Danuphan"><img alt="GitHub Logo" src={logo}/></a></li>
+                        <li><p>Maciej</p><a href="https://github.com/Machkeck"><img alt="GitHub Logo" src={logo}/></a></li>
+                    </ul>
+                </div>
             </div>
         );
     }
